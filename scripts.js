8f79b40765cf2c732a311828f3bdc1f18ca6eeef
Daniel Chrobak <danielchrobak12@gmail.com>
2024-08-21 18:09:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 let transactionData = [];
 let latestChallengeData = [];
 let difficultyCounts = {};
-let difficultyChart;
+let difficultyHistogramChart;
+
+const encodedUrl = "aHR0cHM6Ly9hcGkuaGVsaXVzLnh5ei92MC9hZGRyZXNzZXMvbWluZVhxcERlQmVNUjhiUFFDeXk5VW5lSlpiakZ5d3JhUzNrb1daOFNTSC90cmFuc2FjdGlvbnM/YXBpLWtleT1jNTA0YjQ2NS03ODViLTQ1NjQtYTkzOS1jMDNmYTllYjk2OGY=";
 
 async function getTransactions() {
-    const encodedUrl = "aHR0cHM6Ly9hcGkuaGVsaXVzLnh5ei92MC9hZGRyZXNzZXMvbWluZVhxcERlQmVNUjhiUFFDeXk5VW5lSlpiakZ5d3JhUzNrb1daOFNTSC90cmFuc2FjdGlvbnM/YXBpLWtleT1jNTA0YjQ2NS03ODViLTQ1NjQtYTkzOS1jMDNmYTllYjk2OGY=";
-
     try {
         const apiUrl = atob(encodedUrl);
         const response = await fetch(apiUrl, {
@@ -38,7 +38,6 @@ function updateMostRecentTransaction() {
             const signature = transactionData[0].signature;
             const timestamp = transactionData[0].timestamp;
 
-            // Calculate the time ago
             const now = Math.floor(Date.now() / 1000);
             const timeAgo = now - timestamp;
             const seconds = timeAgo % 60;
@@ -50,6 +49,10 @@ function updateMostRecentTransaction() {
 
             if (timeAgo >= 80) {
                 getTransactions();
+                updateActiveMiners();
+                setTimeout(() => {
+                    console.log("Waiting for data...");
+                }, 2000);
             }
         } else {
             recentTxnElement.textContent = 'No recent transactions available';
@@ -77,12 +80,11 @@ async function updateActiveMiners() {
         console.log('Active Miners Count:', activeMinersCount);
         
         const activeMinersElement = document.getElementById('activeMiners');
-        activeMinersElement.textContent = `${activeMinersCount}`;
+        if (activeMinersElement) activeMinersElement.textContent = `${activeMinersCount}`;
 
         calculateAndDisplayPoolHashrate();
         findAndDisplayHighestDifficulty();
         countDifficulties();
-        updateDifficultyHistogram();
 
     } catch (error) {
         console.error('Error fetching active miners data:', error);
@@ -91,28 +93,17 @@ async function updateActiveMiners() {
 }
 
 function calculateAndDisplayPoolHashrate() {
-    let totalDifficulty = 0;
+    if (latestChallengeData.length === 0) return;
 
-    latestChallengeData.forEach(miner => {
-        totalDifficulty += miner.difficulty;
-    });
-
+    const totalDifficulty = latestChallengeData.reduce((sum, miner) => sum + miner.difficulty, 0);
     const avgDifficulty = totalDifficulty / latestChallengeData.length;
     const avgHashrate = (Math.pow(2, avgDifficulty)) / 60;
     const totalHashrate = avgHashrate * latestChallengeData.length;
-
     const roundedHashrate = Math.round(totalHashrate);
 
     console.log('Estimated Pool Hashrate:', roundedHashrate);
-
-    if (totalHashrate !== 0) {
-        const totalHashrateElement = document.getElementById('totalHashrate');
-        if (totalHashrateElement) {
-            totalHashrateElement.textContent = `${roundedHashrate.toLocaleString()} H/s`;
-        } else {
-            console.error('Element with ID "totalHashrate" not found.');
-        }
-    }
+    const totalHashrateElement = document.getElementById('totalHashrate');
+    if (totalHashrateElement) totalHashrateElement.textContent = `${roundedHashrate.toLocaleString()} H/s`;
 }
 
 function findAndDisplayHighestDifficulty() {
@@ -121,21 +112,11 @@ function findAndDisplayHighestDifficulty() {
         return;
     }
 
-    const latestDifficulty = latestChallengeData.reduce((max, miner) => {
-        return miner.difficulty > max ? miner.difficulty : max;
-    }, 0);
-
-    console.log('Latest Pool Difficulty:', latestDifficulty)
-
+    const latestDifficulty = latestChallengeData.reduce((max, miner) => Math.max(max, miner.difficulty), 0);
+    console.log('Latest Pool Difficulty:', latestDifficulty);
     const difficultyElement = document.getElementById('latestDifficulty');
-    if (difficultyElement) {
-        difficultyElement.textContent = `${latestDifficulty}`;
-    } else {
-        console.error('Element with ID "latestDifficulty" not found.');
-    }
+    if (difficultyElement) difficultyElement.textContent = `${latestDifficulty}`;
 }
-
-let difficultyHistogramChart;
 
 function countDifficulties() {
     if (latestChallengeData.length === 0) {
@@ -143,16 +124,10 @@ function countDifficulties() {
         return;
     }
 
-    difficultyCounts = {};
-
-    latestChallengeData.forEach(miner => {
-        const difficulty = miner.difficulty;
-        if (difficulty in difficultyCounts) {
-            difficultyCounts[difficulty]++;
-        } else {
-            difficultyCounts[difficulty] = 1;
-        }
-    });
+    difficultyCounts = latestChallengeData.reduce((counts, miner) => {
+        counts[miner.difficulty] = (counts[miner.difficulty] || 0) + 1;
+        return counts;
+    }, {});
 
     console.log('Difficulty counts:', difficultyCounts);
 
@@ -160,14 +135,11 @@ function countDifficulties() {
     const countValues = Object.values(difficultyCounts);
 
     const ctxDifficultyCount = document.getElementById('difficultyHistogram').getContext('2d');
-
     const gradientDifficultyCount = ctxDifficultyCount.createLinearGradient(0, 0, 0, 400);
     gradientDifficultyCount.addColorStop(0, 'rgba(153, 102, 255, 0.7)');
     gradientDifficultyCount.addColorStop(1, 'rgba(153, 102, 255, 0)');
 
-    if (difficultyHistogramChart) {
-        difficultyHistogramChart.destroy();
-    }
+    if (difficultyHistogramChart) difficultyHistogramChart.destroy();
 
     difficultyHistogramChart = new Chart(ctxDifficultyCount, {
         type: 'bar',
@@ -187,39 +159,21 @@ function countDifficulties() {
                     display: true,
                     text: 'Difficulty Histogram From Latest Mine Submissions',
                     color: '#fff',
-                    font: {
-                        size: 16,
-                        weight: 'bold'
-                    },
-                    padding: {
-                        top: 10,
-                        bottom: 30
-                    }
+                    font: { size: 16, weight: 'bold' },
+                    padding: { top: 10, bottom: 30 }
                 },
-                tooltip: {
-                    enabled: true
-                },
-                legend: {
-                    display: false,
-                }
+                tooltip: { enabled: true },
+                legend: { display: false }
             },
             scales: {
                 y: {
                     beginAtZero: true,
-                    grid: {
-                        color: 'rgba(255, 255, 255, 0.1)',
-                    },
-                    ticks: {
-                        color: '#fff',
-                    }
+                    grid: { color: 'rgba(255, 255, 255, 0.1)' },
+                    ticks: { color: '#fff' }
                 },
                 x: {
-                    grid: {
-                        color: 'rgba(255, 255, 255, 0.1)',
-                    },
-                    ticks: {
-                        color: '#fff',
-                    }
+                    grid: { color: 'rgba(255, 255, 255, 0.1)' },
+                    ticks: { color: '#fff' }
                 }
             }
         }
@@ -228,6 +182,7 @@ function countDifficulties() {
 
 function updateDashboard() {
     updateActiveMiners();
+    updateMostRecentTransaction();
 }
 
 getTransactions();
