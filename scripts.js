16af469c3f03cdf6a2ac03a98ef10ecbf44b6e60
Daniel Chrobak <danielchrobak12@gmail.com>
2024-08-21 17:40:50 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,126 +1,7 @@
-const ctxHashrate = document.getElementById('hashrateChart').getContext('2d');
-const gradientHashrate = ctxHashrate.createLinearGradient(0, 0, 0, 400);
-gradientHashrate.addColorStop(0, 'rgba(0, 255, 127, 0.7)');
-gradientHashrate.addColorStop(1, 'rgba(0, 255, 127, 0)');
-
-const hashrateData = {
-    labels: ['0h', '2h', '4h', '6h', '8h', '10h', '12h'],
-    datasets: [{
-        label: 'Hashrate (TH/s)',
-        data: [1.0, 1.1, 1.2, 1.15, 1.2, 1.18, 1.2],
-        backgroundColor: gradientHashrate,
-        borderColor: 'rgba(0, 255, 127, 1)',
-        borderWidth: 2,
-        tension: 0.4,
-        pointRadius: 0
-    }]
-};
-
-new Chart(ctxHashrate, {
-    type: 'line',
-    data: hashrateData,
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-                grid: {
-                    color: 'rgba(255, 255, 255, 0.1)',
-                },
-                ticks: {
-                    color: '#fff',
-                }
-            },
-            x: {
-                grid: {
-                    color: 'rgba(255, 255, 255, 0.1)',
-                },
-                ticks: {
-                    color: '#fff',
-                }
-            }
-        },
-        plugins: {
-            tooltip: {
-                backgroundColor: 'rgba(0, 0, 0, 0.8)',
-                titleColor: '#00ff7f',
-                bodyColor: '#fff',
-                cornerRadius: 4,
-                padding: 10
-            },
-            legend: {
-                display: false,
-            }
-        },
-        elements: {
-            line: {
-                tension: 0.4
-            }
-        }
-    }
-});
-
-const ctxEarnings = document.getElementById('earningsChart').getContext('2d');
-const gradientEarnings = ctxEarnings.createLinearGradient(0, 0, 0, 400);
-gradientEarnings.addColorStop(0, 'rgba(0, 255, 127, 0.7)');
-gradientEarnings.addColorStop(1, 'rgba(0, 255, 127, 0)');
-
-const earningsData = {
-    labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7'],
-    datasets: [{
-        label: 'Earnings (BTC)',
-        data: [10, 15, 12, 14, 20, 25, 30],
-        backgroundColor: gradientEarnings,
-        borderColor: 'rgba(0, 255, 127, 1)',
-        borderWidth: 2,
-        tension: 0.4,
-        pointRadius: 0
-    }]
-};
-
-new Chart(ctxEarnings, {
-    type: 'line',
-    data: earningsData,
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-                grid: {
-                    color: 'rgba(255, 255, 255, 0.1)',
-                },
-                ticks: {
-                    color: '#fff',
-                }
-            },
-            x: {
-                grid: {
-                    color: 'rgba(255, 255, 255, 0.1)',
-                },
-                ticks: {
-                    color: '#fff',
-                }
-            }
-        },
-        plugins: {
-            tooltip: {
-                backgroundColor: 'rgba(0, 0, 0, 0.8)',
-                titleColor: '#00ff7f',
-                bodyColor: '#fff',
-                cornerRadius: 4,
-                padding: 10
-            },
-            legend: {
-                display: false,
-            }
-        },
-        elements: {
-            line: {
-                tension: 0.4
-            }
-        }
-    }
-});
-
 let transactionData = [];
+let latestChallengeData = [];
+let difficultyCounts = {};
+let difficultyChart;
 
 async function getTransactions() {
     const encodedUrl = "aHR0cHM6Ly9hcGkuaGVsaXVzLnh5ei92MC9hZGRyZXNzZXMvbWluZVhxcERlQmVNUjhiUFFDeXk5VW5lSlpiakZ5d3JhUzNrb1daOFNTSC90cmFuc2FjdGlvbnM/YXBpLWtleT1jNTA0YjQ2NS03ODViLTQ1NjQtYTkzOS1jMDNmYTllYjk2OGY=";
@@ -176,8 +57,6 @@ function updateMostRecentTransaction() {
     }
 }
 
-let latestChallengeData = [];
-
 async function updateActiveMiners() {
     const url = 'https://domainexpansion.tech/last-challenge-submissions';
     
@@ -202,6 +81,8 @@ async function updateActiveMiners() {
 
         calculateAndDisplayPoolHashrate();
         findAndDisplayHighestDifficulty();
+        countDifficulties();
+        updateDifficultyHistogram();
 
     } catch (error) {
         console.error('Error fetching active miners data:', error);
@@ -234,7 +115,6 @@ function calculateAndDisplayPoolHashrate() {
     }
 }
 
-
 function findAndDisplayHighestDifficulty() {
     if (latestChallengeData.length === 0) {
         console.warn('No challenge data available to find the highest difficulty.');
@@ -253,6 +133,97 @@ function findAndDisplayHighestDifficulty() {
     } else {
         console.error('Element with ID "latestDifficulty" not found.');
     }
+}
+
+let difficultyHistogramChart;
+
+function countDifficulties() {
+    if (latestChallengeData.length === 0) {
+        console.warn('No challenge data available.');
+        return;
+    }
+
+    difficultyCounts = {};
+
+    latestChallengeData.forEach(miner => {
+        const difficulty = miner.difficulty;
+        if (difficulty in difficultyCounts) {
+            difficultyCounts[difficulty]++;
+        } else {
+            difficultyCounts[difficulty] = 1;
+        }
+    });
+
+    console.log('Difficulty counts:', difficultyCounts);
+
+    const countLabels = Object.keys(difficultyCounts);
+    const countValues = Object.values(difficultyCounts);
+
+    const ctxDifficultyCount = document.getElementById('difficultyHistogram').getContext('2d');
+
+    const gradientDifficultyCount = ctxDifficultyCount.createLinearGradient(0, 0, 0, 400);
+    gradientDifficultyCount.addColorStop(0, 'rgba(153, 102, 255, 0.7)');
+    gradientDifficultyCount.addColorStop(1, 'rgba(153, 102, 255, 0)');
+
+    if (difficultyHistogramChart) {
+        difficultyHistogramChart.destroy();
+    }
+
+    difficultyHistogramChart = new Chart(ctxDifficultyCount, {
+        type: 'bar',
+        data: {
+            labels: countLabels,
+            datasets: [{
+                label: 'Difficulty Count',
+                data: countValues,
+                backgroundColor: gradientDifficultyCount,
+                borderColor: 'rgba(153, 102, 255, 1)',
+                borderWidth: 2,
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Difficulty Histogram From Latest Pool Mine Submission',
+                    color: '#fff',
+                    font: {
+                        size: 16,
+                        weight: 'bold'
+                    },
+                    padding: {
+                        top: 10,
+                        bottom: 30
+                    }
+                },
+                tooltip: {
+                    enabled: true
+                },
+                legend: {
+                    display: false,
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.1)',
+                    },
+                    ticks: {
+                        color: '#fff',
+                    }
+                },
+                x: {
+                    grid: {
+                        color: 'rgba(255, 255, 255, 0.1)',
+                    },
+                    ticks: {
+                        color: '#fff',
+                    }
+                }
+            }
+        }
+    });
 }
 
 function updateDashboard() {
